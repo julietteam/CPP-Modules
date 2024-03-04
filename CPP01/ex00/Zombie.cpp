b06f67f1e2db25c1febe2f76e4aa7276062f350e
Juliette ANDRIEUX <juandrie@paul-f5Br1s1.clusters.42paris.fr>
2024-03-04 18:18:27 +0100

    ajout 1er exo

---
+++ CPP01/ex00/Zombie.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juandrie <juandrie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/04 17:54:28 by juandrie          #+#    #+#             */
+/*   Updated: 2024/03/04 18:10:16 by juandrie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+#include <iostream>
+
+Zombie::Zombie(std::string name) : name(name) {
+}
+
+Zombie::~Zombie() {
+    std::cout << name << " has been deleted." << std::endl;
+}
+
+void Zombie::announce() const
+{
+    std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
---
+++ CPP01/ex00/randomChump.cpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juandrie <juandrie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/04 17:57:03 by juandrie          #+#    #+#             */
+/*   Updated: 2024/03/04 17:57:25 by juandrie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+void randomChump(std::string name)
+{
+    Zombie tempZombie(name);
+    tempZombie.announce();
+}
