2c31b9be029f4085002b4ca107139121a5b156d1
Juliette ANDRIEUX <juandrie@paul-f5Br1s3.clusters.42paris.fr>
2024-03-06 15:30:44 +0100

    ajouts divers

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,7 +6,20 @@
 /*   By: juandrie <juandrie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 12:09:55 by juandrie          #+#    #+#             */
-/*   Updated: 2024/03/06 12:09:56 by juandrie         ###   ########.fr       */
+/*   Updated: 2024/03/06 12:49:32 by juandrie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "filename.hpp"
+
+int main(int ac, char **av)
+{
+    std:: string s1;
+    std:: string s2;
+
+    if (ac != 4)
+    {
+        std::cerr << "Usage: " << argv[0] << " <filename> <string to replace> <string to replace with>" << std::endl;
+        return 1;
+    }
+}
